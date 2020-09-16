1804c69c3dccfe80472a3e3199deddd7535d3d7b
macintoshhd <1234>
2020-09-16 18:01:45 +0700

    first commit

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -1,13 +1,12 @@
 
 import UIKit
 
-class ToDoListViewController: UIViewController {
+class ToDoListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
