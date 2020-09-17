a514c5deefa400989a4a13a0a07173adcc7127f3
macintoshhd <1234>
2020-09-17 13:13:26 +0700

    add list persist

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -2,11 +2,64 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-
+    
+    var myItem = ["Hello", "How are you ?" , "Good morning !"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let library_path = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0]
+        print("library path is \(library_path)")
         // Do any additional setup after loading the view.
+        if let items = UserDefaults.standard.array(forKey: "listItem") as? [String] {
+            myItem = items
+        }
     }
     
+    //    Add Item
+    @IBAction func addNewItem(_ sender: UIBarButtonItem) {
+        
+        var textFieldResult = UITextField()
+        
+        let alert = UIAlertController(title: "Please enter your item!", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "add", style: .default, handler: {
+            (action) in
+            //code
+            print("\(textFieldResult.text ?? "")")
+            DispatchQueue.main.async {
+                self.myItem.append(textFieldResult.text!)
+                UserDefaults.standard.set(self.myItem, forKey: "listItem")
+                self.tableView.reloadData()
+            }
+        })
+        
+        alert.addTextField(configurationHandler: {
+            (textField) in
+            textField.placeholder = "Some item name"
+            textFieldResult = textField
+        })
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: {})
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myItem.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoList", for: indexPath)
+        
+        cell.textLabel?.text = myItem[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if(tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    }
 }
 
